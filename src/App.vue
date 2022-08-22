6c4ec220c1a6c8483e125c8116f1df71231e9cfa
Jimena Miguez <jimeblau@gmail.com>
2022-08-22 16:59:26 +0200

    Finish slots

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,52 @@
 <template>
+  <!-- Slots are used to create templates that we can pass inside a component.
+     In this example, I create the formHelper component, which I want to use
+     to create a form inside my website.-->
   <div>
-    <Header />
-    <Ninjas />
-    <Footer />
+    <!-- 1)I use the component form-helper with open and closing tag -->
+    <!-- 2)Inside the componet use, I create my desired template by means of the 
+           slot attribute with a name. For example: slot="form-header"  -->
+    <!-- 3) In the componet form-helper.vue, I use the slots to display the
+            template using the following structure  <slot name="form-header"></slot>-->
+    <!-- IMPORTANT: if I want some data to be dynamic, I need to have that
+            data in this component and not in the form-helper component -->
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of a form</h3>
+        <p>This is some info about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
-import Ninjas from "./components/Ninjas.vue";
+// Imports
+import formHelper from "./components/formHelper.vue";
 export default {
   components: {
-    Header,
-    Footer,
-    Ninjas
+    "form-helper": formHelper
   },
-  data() {}
+  data() {
+    return {};
+  },
+  methods: {
+    handleSubmit: function() {
+      alert("thanks for submitting");
+    }
+  }
 };
 </script>
 
 <style>
-
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
 </style>
