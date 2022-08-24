d204adc576d35b5fd34fcef4cf8ce13412b5b9ed
Jimena Miguez <jimeblau@gmail.com>
2022-08-24 13:52:49 +0200

    Finish custom search filter

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,9 +1,16 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <!-- EXPLANATION: create a filter input so that the user can search the posts that
+  include the word typed in the filter -->
+  <div id="show-blogs">
     <h1>All Blog Articles</h1>
-    <!-- IMPORTANT: don´t pay attention to errors in <li>, it´s just because there is no key binding -->
-    <div v-for="blog in blogs" class="single-blog">
-      <h2 v-rainbow | to-uppercase>{{ blog.title }}</h2>
+    <!-- 1) Create a input where the user can type a word to filter the data.
+    Using the v-model directive, store the whatever the user types in a 
+    variable called "search" -->
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <!-- 7) Now, instead of looping throung blogs I have to loop through the 
+    array with the filtered data, i.e  filteredBlogs-->
+    <div v-for="blog in filteredBlogs" class="single-blog">
+      <h2>{{ blog.title | (to - uppercase) }}</h2>
       <article>{{ blog.body }}</article>
     </div>
   </div>
@@ -13,7 +20,10 @@
 export default {
   data() {
     return {
-      blogs: []
+      blogs: [],
+      /* 2) Add the variable "search" to store whatever the user types in
+      the filter input*/
+      search: ""
     };
   },
   methods: {},
@@ -21,9 +31,23 @@ export default {
     this.$http
       .get("http://jsonplaceholder.typicode.com/posts")
       .then(function(data) {
-        console.log(data);
         this.blogs = data.body.slice(0, 10);
       });
+  },
+  /* 3) Create the filter functionallity inside a computed property, 
+   because the filtered data depends on my original data*/
+  computed: {
+    /* 4) Create a function called "filteredBlogs", which will return an array
+    with the filtered data */
+    filteredBlogs: function() {
+      /* 5)Use the filter method to filter the data, i.e to filter the blogs */
+      return this.blogs.filter(blog => {
+        /* 6)For every blog, the filter method checks if the blog´s title 
+        matched whatever the user typed in the filter input (this.search).
+        If it is true, it keeps it, and if it´s false it eleminates it. */
+        return blog.title.match(this.search);
+      });
+    }
   }
 };
 </script>
