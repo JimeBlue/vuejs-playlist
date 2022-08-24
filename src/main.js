a491d525ceea002289fa7aea5570c4069d47425f
Jimena Miguez <jimeblau@gmail.com>
2022-08-24 13:13:54 +0200

    Finish filters

--- src/main.js
+++ src/main.js
@@ -29,6 +29,11 @@ Vue.directive("theme", {
   }
 });
 
+// Filters
+Vue.filter("to-uppercase", function(value) {
+  return value.toUpperCase();
+});
+
 new Vue({
   el: "#app",
   render: h => h(App)
