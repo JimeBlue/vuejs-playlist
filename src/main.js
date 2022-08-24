93c6d4d20e595684f7557a02c3a549e03eaf15d5
Jimena Miguez <jimeblau@gmail.com>
2022-08-24 16:07:11 +0200

    delete comments

--- src/main.js
+++ src/main.js
@@ -9,14 +9,6 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: Routes,
-  /* EXPLANATION: there are 2 types of routing modes, hash and history.
-   With hash, after the foward slash we get a hash (#). By deafult,
-   Vue used the hash mode. If we want to change it, ´cause it really
-   looks wired to have a # in the url, we need to to add mode:history,
-   like I did below. This can also be done directly on the server and
-   there is a great explanation of what all of this happens in the net
-   ninja Vue2 tutorial video #40
-   (https://www.youtube.com/watch?v=js5Sr_ZSHDs&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=41)*/
   mode: "history"
 });
 
