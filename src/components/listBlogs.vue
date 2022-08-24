d3fd60117bbae7b84375f282577c6708cce7ce44
Jimena Miguez <jimeblau@gmail.com>
2022-08-24 14:42:35 +0200

    Finich mixins

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="show-blogs">
-    <h1>All Blog Articles</h1>
+    <h1>List Blog Titles</h1>
     <input type="text" v-model="search" placeholder="search blogs" />
     <div v-for="blog in filteredBlogs" class="single-blog">
-      <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
-      <article>{{ blog.body }}</article>
+      <h2>{{ blog.title }}</h2>
     </div>
   </div>
 </template>
 
 <script>
+// Imports
+import searchMixin from "../mixins/searchMixin";
 export default {
   data() {
     return {
@@ -17,7 +18,6 @@ export default {
       search: ""
     };
   },
-  methods: {},
   created() {
     this.$http
       .get("http://jsonplaceholder.typicode.com/posts")
@@ -25,29 +25,7 @@ export default {
         this.blogs = data.body.slice(0, 10);
       });
   },
-  computed: {
-    filteredBlogs: function() {
-      return this.blogs.filter(blog => {
-        return blog.title.match(this.search);
-      });
-    }
-  },
-  filters: {
-    toUppercase(value) {
-      return value.toUpperCase();
-    }
-  },
-  directives: {
-    rainbow: {
-      bind(el, binding, vnode) {
-        el.style.color =
-          "#" +
-          Math.random()
-            .toString(16)
-            .slice(2, 8);
-      }
-    }
-  }
+  mixins: [searchMixin]
 };
 </script>
 
