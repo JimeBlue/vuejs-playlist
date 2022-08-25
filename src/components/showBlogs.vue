bc11ac8d78fcb279d7e5f56911d8ddc376d75810
Jimena Miguez <jimeblau@gmail.com>
2022-08-25 11:30:10 +0200

    Finish route parameters

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -3,7 +3,20 @@
     <h1>All Blog Articles</h1>
     <input type="text" v-model="search" placeholder="search blogs" />
     <div v-for="blog in filteredBlogs" class="single-blog">
-      <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
+      <!-- 7) To allow the user to click in a blog and go to it,
+      wrap the <h2> containing the title with a  <router-link>
+      tag. Inside the <router-link> add the attribute "to" in
+      order to indicate where the link shoulg go. The value
+      of that attribure must be the individual id of this
+      blog. So,  I need data binding to set the "to" attribute
+      to the object blog + the attribute id, which is store in
+      my data in this component. The result is: v-bind:to="'/blog/' + blog.id.
+      It´s important to remember that I requested the data from
+      the database in this component and that with that data came an id. -->
+      <!-- 8) The end! Now, the title of each blog is a link 😇  -->
+      <router-link v-bind:to="'/blog/' + blog.id"
+        ><h2>{{ blog.title }}</h2></router-link
+      >
       <article>{{ blog.body }}</article>
     </div>
   </div>
